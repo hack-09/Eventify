8d34e5c1494ca4905cc9f5edc28e8f54a5841daa
Priyanshu Kumar <priyanshukumar9780@gmail.com>
2025-01-22 14:55:15 +0530

    refactor: clean up formatting and improve code readability in EventCard component

--- frontend/src/components/EventCard/EventCard.js
+++ frontend/src/components/EventCard/EventCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from 'axios';
-import {  deleteEvent, joiningEvent } from "../../utils/api";
+import { deleteEvent, joiningEvent } from "../../utils/api";
 import { Link, useNavigate } from "react-router-dom";
 import { getUserIdFromToken } from '../../utils/tokenHelper';
 import './EventCard.css';
@@ -10,16 +10,16 @@ const EventCard = ({ event, showManagementOptions, onEventUpdated }) => {
 
   const handleDelete = async () => {
     if (window.confirm("Are you sure you want to delete this event?")) {
-        try {
-            await deleteEvent(event._id);
-            if (onEventUpdated) {
-                onEventUpdated(event._id); // Pass the deleted event ID
-            }
-        } catch (err) {
-            console.error("Failed to delete event:", err);
+      try {
+        await deleteEvent(event._id);
+        if (onEventUpdated) {
+          onEventUpdated(event._id); // Pass the deleted event ID
         }
+      } catch (err) {
+        console.error("Failed to delete event:", err);
+      }
     }
-}; 
+  };
 
   const joinEvent = async (eventId) => {
     const userId = getUserIdFromToken(); // Extract userId from token
@@ -27,9 +27,9 @@ const EventCard = ({ event, showManagementOptions, onEventUpdated }) => {
       alert('Please log in to join events.');
       return;
     }
-  
+
     try {
-      const response = await joiningEvent(eventId,userId);
+      const response = await joiningEvent(eventId, userId);
       alert(response.data.message);
       console.log("Successfully joined event:", response.data);
       navigate(`/event/${event._id}/join`);
@@ -46,8 +46,8 @@ const EventCard = ({ event, showManagementOptions, onEventUpdated }) => {
       <p>{event.description}</p>
       <p>{new Date(event.date).toLocaleString()}</p>
       <p>Category: {event.category}</p>
-      
-      
+
+
       {showManagementOptions && <Link to={`/event/edit/${event._id}`}>Edit Event</Link>}
       {showManagementOptions && <button type="button" onClick={handleDelete}>Delete</button>}
       <Link to={`/event/${event._id}`}>Event Details</Link>
